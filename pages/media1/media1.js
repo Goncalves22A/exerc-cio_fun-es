2f9cc33093daf189e465cbfb7210ec7b094cbcaf
Melissa Gonçalves <melissagoncalves699@gmail.com>
2025-05-03 18:06:53 -0300

    resolução do exercício media 1

---
+++ pages/media1/media1.js
@@ -0,0 +1,13 @@
+function media (nota1, nota2){
+    return (nota1 * 3.5 + nota2 * 7.5) /11
+}
+
+let media1 =  media(5, 7.1)
+let media2 =  media(0, 7.1)
+let media3 =  media(10, 10)
+
+console.log(`
+    media1 = ${media1.toFixed(5)}
+    media2 = ${media2.toFixed(5)}
+    media3 = ${media3.toFixed(5)}
+    `)
