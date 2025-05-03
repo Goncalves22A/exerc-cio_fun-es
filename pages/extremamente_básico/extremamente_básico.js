ee506e1d38b486e8d1e44896e02b1853551c4215
Melissa Gonçalves <melissagoncalves699@gmail.com>
2025-05-03 17:44:15 -0300

    resolução do exercício extremamente básico

---
+++ pages/extremamente_básico/extremamente_básico.js
@@ -0,0 +1,14 @@
+function somar (valor1, valor2){
+    return valor1 + valor2
+}
+
+teste1 = somar(10, 9)
+teste2 = somar (-10, 4)
+teste3 = somar (15, -7)
+
+console.log(`
+X1 =  ${teste1}
+X2 =  ${teste2}
+X3 =  ${teste3}
+
+    `)
