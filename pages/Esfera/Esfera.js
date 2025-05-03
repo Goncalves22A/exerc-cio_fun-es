20750ccecc61f33371fccf6ae2e5ccab8545edbf
Melissa Gonçalves <melissagoncalves699@gmail.com>
2025-05-03 19:34:42 -0300

    resolução do exercício esfera

---
+++ pages/Esfera/Esfera.js
@@ -0,0 +1,11 @@
+function esfera(R){
+   return  (4.0/3) * 3.14159 * R*R*R
+}
+
+let volume1 = esfera(3)
+let volume2 = esfera(15)
+let volume3 = esfera(1523)
+
+console.log(`volume 1 = ${volume1}`)
+console.log(`volume 2 = ${volume2}`)
+console.log(`volume 3 = ${volume3}`)
